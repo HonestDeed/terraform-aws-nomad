64906993663ac7a6692c54028ad94726f64cc71d
cjbarre <cjbarre@gmail.com>
2022-03-09 15:53:36 -0800

    Remove var.

--- variables.tf
+++ variables.tf
@@ -19,12 +19,6 @@
 # These parameters have reasonable defaults.
 # ---------------------------------------------------------------------------------------------------------------------
 
-variable "ami_id" {
-  description = "The ID of the AMI to run in the cluster. This should be an AMI built from the Packer template under examples/nomad-consul-ami/nomad-consul.json. If no AMI is specified, the template will 'just work' by using the example public AMIs. WARNING! Do not use the example AMIs in a production setting!"
-  type        = string
-  default     = ""
-}
-
 variable "cluster_name" {
   description = "What to name the cluster and all of its associated resources"
   type        = string
